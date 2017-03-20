6b110e4395285f8373fbce87a6840e6ed0ab81e2
root <420780215@qq.com>
2017-03-20 12:42:45 +0800

    1

--- index.php
+++ index.php
@@ -2,13 +2,14 @@
 	<head>
 		<meta charset="utf-8"/>
 		<title>LQ的个人博客</title>
-	<link rel="stylesheet" type="text/css" href="css/main.css"/>
+		<link rel="stylesheet" href="css/css.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css"/>
 	</head>
 	<body>
 		<div id="main">
 			<div id="menu">
 				<ul>
-					<li><h2 class="logo">LQ的个人微博</h2></li>
+					<li><h2 id="logo">LQ的个人微博</h2></li>
 					<li><a href="#" class="active">首页</a></li>
 					<li><a href="sfdc">Salesforce</a></li>
 					<li><a href="life">生活</a></li>
