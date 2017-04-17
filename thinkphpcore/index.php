f56663c72ef3a720401d25c401750ca54df430c4
root <420780215@qq.com>
2017-04-17 10:40:58 +0800

    1

--- thinkphpcore/index.php
+++ thinkphpcore/index.php
@@ -17,6 +17,10 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',true);
 
+define('BLOG_IMG_URL','./Application/Public/image');
+define('BLOG_CSS_URL','/Application/Public/css');
+define('BLOG_JS_URL','/Application/Public/js');
+
 // 定义应用目录
 define('APP_PATH','./Application/');
 
